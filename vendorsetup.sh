dbd03238ff8c7513e1315828c0f0a05cf49df802
Master <mhmdeveloperofficial@gmail.com>
2022-06-21 20:40:15 +0530

    guacamole: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,32 @@
+# Clone/Fetch Upstream Device Dependencies
+# Device Tree
+echo "Cloning OnePlus 7Pro Tree"
+git clone https://github.com/PixelPlusUI-devices/device_oneplus_guacamole device/oneplus/guacamole
+git clone https://github.com/PixelPlusUI-devices/device_oneplus_sm8150-common device/oneplus/sm8150-common/
+echo ""
+
+echo "Cloning Vendor blobs"
+git clone https://github.com/PixelPlusUI-devices/vendor_oneplus_guacamole vendor/oneplus/guacamole
+git clone https://github.com/PixelPlusUI-devices/vendor_oneplus_sm8150-common vendor/oneplus/sm8150-common/
+echo ""
+
+echo "Cloning kernel tree"
+git clone https://github.com/PixelPlusUI-devices/kernel_oneplus_guacamole kernel/oneplus/sm8150
+echo ""
+
+echo "Cloning Hardware/Oneplus"
+git clone https://github.com/mhmdeve/hardware_oneplus hardware/oneplus
+
+# OnePlus Stuff 
+echo "Cloning OnePlus"
+git clone https://gitlab.com/PixelPlusUI-SnowCone/vendor_oneplus_apps vendor/oneplus/apps
+git clone https://github.com/PixelPlusUI-Devices/vendor_motorola_dolby vendor/motorola/dolby
+echo ""
+
+# HALs 
+echo "Cloning HALs"
+rm -rf hardware/qcom-caf/sm8150/audio hardware/qcom-caf/sm8150/display hardware/qcom-caf/sm8150/media
+git clone https://github.com/mhmdeve/hardware_qcom-caf_sm8150_media hardware/qcom-caf/sm8150/media
+git clone https://github.com/mhmdeve/hardware_qcom-caf_sm8150_audio hardware/qcom-caf/sm8150/audio
+git clone https://github.com/mhmdeve/hardware_qcom-caf_sm8150_display hardware/qcom-caf/sm8150/display
+echo ""
