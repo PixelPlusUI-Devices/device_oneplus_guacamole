aa0bf8a712932711b4357b582b8446270c11da5c
Sal Savage <salsavage@google.com>
2022-11-14 22:22:13 +0100

    guacamole: Replace `BTM_DEF_LOCAL_NAME` with a sysprop
    Also:
    Add OnePlus 7T Pro NR Varient
    Import Some props to override
    REF: https://github.com/AOSP-Krypton/device_oneplus_sm8150-common/blob/A12/init/init_oneplus-sm8150.cpp#L230

--- init/init_oneplus-sm8150.cpp
+++ init/init_oneplus-sm8150.cpp
@@ -1,6 +1,5 @@
 /*
    Copyright (c) 2020, The LineageOS Project
-
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -13,7 +12,6 @@
     * Neither the name of The Linux Foundation nor the names of its
       contributors may be used to endorse or promote products derived
       from this software without specific prior written permission.
-
    THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
@@ -50,10 +48,9 @@ void property_override(char const prop[], char const value[]) {
     __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void property_override_multi(char const system_prop[], char const vendor_prop[], char const bootimage_prop[], char const value[]) {
+void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[]) {
     property_override(system_prop, value);
     property_override(vendor_prop, value);
-    property_override(bootimage_prop, value);
 }
 
 void load_dalvikvm_properties() {
@@ -72,25 +69,18 @@ void load_dalvikvm_properties() {
     property_override("dalvik.vm.heapgrowthlimit", "256m");
     property_override("dalvik.vm.heaptargetutilization", "0.46");
     property_override("dalvik.vm.heapmaxfree", "48m");
-    }
-  else {
-    // from - phone-xhdpi-6144-dalvik-heap.mk
-    property_override("dalvik.vm.heapstartsize", "16m");
-    property_override("dalvik.vm.heapgrowthlimit", "256m");
-    property_override("dalvik.vm.heaptargetutilization", "0.5");
-    property_override("dalvik.vm.heapmaxfree", "32m");
   }
-  property_override("dalvik.vm.heapsize", "512m");
-  property_override("dalvik.vm.heapminfree", "8m");
 }
 
 void vendor_load_properties() {
+  int prj_version = stoi(android::base::GetProperty("ro.boot.prj_version", ""));
   int project_name = stoi(android::base::GetProperty("ro.boot.project_name", ""));
   int rf_version = stoi(android::base::GetProperty("ro.boot.rf_version", ""));
-  switch (project_name) {
+  switch(project_name){
     case 18857:
       /* OnePlus 7 */
-      switch (rf_version) {
+      property_override("bluetooth.device.default_name", "OnePlus 7");
+      switch (rf_version){
         case 1:
           /* China */
           property_override("ro.product.model", "GM1900");
@@ -104,7 +94,6 @@ void vendor_load_properties() {
           property_override("ro.product.model", "GM1903");
           break;
         case 5:
-        default:
           /* Global / US Unlocked */
           property_override("ro.product.model", "GM1905");
           break;
@@ -112,7 +101,8 @@ void vendor_load_properties() {
       break;
     case 18821:
       /* OnePlus 7 Pro */
-      switch (rf_version) {
+      property_override("bluetooth.device.default_name", "OnePlus 7 Pro");
+      switch (rf_version){
         case 1:
           /* China */
           property_override("ro.product.model", "GM1910");
@@ -126,7 +116,6 @@ void vendor_load_properties() {
           property_override("ro.product.model", "GM1913");
           break;
         case 5:
-        default:
           /* Global / US Unlocked */
           property_override("ro.product.model", "GM1917");
           break;
@@ -134,11 +123,13 @@ void vendor_load_properties() {
       break;
     case 18831:
       /* OnePlus 7 Pro T-Mobile */
+      property_override("bluetooth.device.default_name", "OnePlus 7 Pro");
       property_override("ro.product.model", "GM1915");
       break;
     case 18865:
       /* OnePlus 7T */
-      switch (rf_version) {
+      property_override("bluetooth.device.default_name", "OnePlus 7T");
+      switch (rf_version){
         case 1:
           /* China */
           property_override("ro.product.model", "HD1900");
@@ -152,7 +143,6 @@ void vendor_load_properties() {
           property_override("ro.product.model", "HD1903");
           break;
         case 5:
-        default:
           /* Global / US Unlocked */
           property_override("ro.product.model", "HD1905");
           break;
@@ -160,11 +150,13 @@ void vendor_load_properties() {
       break;
     case 19863:
       /* OnePlus 7T T-Mobile */
+      property_override("bluetooth.device.default_name", "OnePlus 7T");
       property_override("ro.product.model", "HD1907");
       break;
     case 19801:
       /* OnePlus 7T Pro */
-      switch (rf_version) {
+      property_override("bluetooth.device.default_name", "OnePlus 7T Pro");
+      switch (rf_version){
         case 1:
           /* China */
           property_override("ro.product.model", "HD1910");
@@ -178,13 +170,23 @@ void vendor_load_properties() {
           property_override("ro.product.model", "HD1913");
           break;
         case 5:
-        default:
           /* Global / US Unlocked */
           property_override("ro.product.model", "HD1917");
           break;
       }
       break;
+    case 19861:
+      /* OnePlus 7T Pro NR */
+      property_override("bluetooth.device.default_name", "OnePlus 7T Pro 5G");
+      property_override("persist.radio.multisim.config", "ssss");
+      property_override("ro.product.model", "HD1925");
+      break;
     }
+
+    property_override("vendor.boot.prj_version", std::to_string(prj_version).c_str());
+    property_override_dual("vendor.rf.version", "vendor.boot.rf_version", std::to_string(rf_version).c_str());
+    property_override_dual("ro.odm.build.type", "ro.product.build.type", "user");
+    property_override_dual("ro.system.build.type", "ro.system_ext.build.type", "user");
 
   // dalvikvm props
   load_dalvikvm_properties();
