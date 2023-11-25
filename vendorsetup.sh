f5e133ca13416edd23a0f35e6b967eb3b60ea657
Master <mhmdeveloperofficial@gmail.com>
2023-11-25 13:35:46 +0000

    guacamole: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/PixelPlusUI-Devices/vendor_oneplus_guacamole vendor/oneplus/guacamole
-git clone https://github.com/PixelPlusUI-Devices/kernel_oneplus_sm8150 kernel/oneplus/sm8150
+git clone https://github.com/PixelPlusUI-Devices/kernel_oneplus_sm8150 kernel/oneplus/sm8150 --depth=1
 git clone https://gitlab.com/PixelPlusUI-SnowCone/vendor_oneplus_apps vendor/oneplus/apps
+git clone https://github.com/LineageOS/android_hardware_lineage_livedisplay hardware/lineage/livedisplay
